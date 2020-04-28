26227a5ea0be7e33ef47ee9ab010ab7f75f14de5
beenjammin <btjensen@mtu.edu>
2020-04-28 15:33:34 -0700

    Dojo

---
+++ Web_Fundamentals/JS/basic_assignments/algos.js
@@ -0,0 +1,3 @@
+var _ = {'Name':'Ben'}
+
+console.log(_.Name);
---
+++ Web_Fundamentals/JS/jQuery_functions/myscripts.js
@@ -0,0 +1,6 @@
+$("button").click(function(){
+    console.log("Button clicked");
+    $("p").slideUp();
+  });
+
+  // Wrap in Document.ready
