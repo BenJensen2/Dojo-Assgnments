159d076e3d4298f58e376e1a9254c4dac8bbf2a6
beenjammin <btjensen@mtu.edu>
2020-04-28 20:28:47 -0700

    jQuery Functions

--- Web_Fundamentals/JS/jQuery_functions/myscripts.js
+++ Web_Fundamentals/JS/jQuery_functions/myscripts.js
@@ -1,6 +1,41 @@
-$("button").click(function(){
-    console.log("Button clicked");
-    $("p").slideUp();
+
+// Makes sure full html script is loaded before execution
+$(document).ready(function(){
+
+  // Click and creates a pop-up alert
+  $(".click button").click(function(){
+    console.log("Click")
+    alert("You are Awesome!");
   });
+
+  $(".hide button").click(function(){
+      console.log("Hide")
+      $(".hide button").hide();
+    });
+
+  $(".show button").click(function(){
+      console.log("Show")
+      $(".hide button").show();
+    });
+
+  $(".toggle button").click(function(){
+    console.log("Toggle")
+    $(".toggle img").toggle();
+  });
+
+  $(".slideDown img").hide(function(){
+    $(".slideDown img").hide()
+  });
+
+  $(".slideDown button").click(function(){
+    console.log("Slide Down")
+    $(".slideDown img").slideDown();
+  });
+
+
+
+});
+
+
 
   // Wrap in Document.ready
