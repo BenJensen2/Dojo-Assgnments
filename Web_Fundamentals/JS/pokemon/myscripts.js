af437773ef7576d67000e52651b8723ba5c7ebcd
beenjammin <btjensen@mtu.edu>
2020-04-30 14:02:25 -0700

    Pokedex Complete

--- Web_Fundamentals/JS/pokemon/myscripts.js
+++ Web_Fundamentals/JS/pokemon/myscripts.js
@@ -2,10 +2,57 @@ $(document).ready(function(){
 
     $.get("https://pokeapi.co/api/v2/pokemon/", function(num) {
         console.log(num.count)
-        for(var i=1;i<10;i++){
-            // $("div.content").append("<img src='' alt='' id=" + i + ">")
-            $("div.content").append("<img src='http://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/" + i + ".png' alt='' id=" + i + ">")
+        var pokePics = "";
+        for(var i=1;i<num.count;i++){
+            // console.log("<img src='http://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/" + i + ".png' alt='' id=" + i + ">")
+            pokePics += "<img src='http://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/" + i + ".png' alt='' id=" + i + ">"
         };
+        // console.log(pokePics)
+
+        $("div.content").html(pokePics);
+
+        $("img").on("click",function(){
+            // console.log(this.id);
+            var pokeID = $(this).attr('id')
+
+            $.get("https://pokeapi.co/api/v2/pokemon/" + pokeID + "/", function(pokemon) {
+                console.log(pokemon)
+                var pokeDescription = "";
+                pokeDescription += `<h1> ${pokemon.name}</h1>`
+                pokeDescription += "<img src='http://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/" + pokeID + ".png'>"
+                pokeDescription += "<h2>Types</h2>"
+                
+                for(j=0; j<pokemon.types.length; j++){
+                    // console.log("For Loop Type Name")
+                    console.log(pokemon.types[j].type.name)
+                    pokeDescription += "<li>" + pokemon.types[j].type.name + "</li>"
+                };
+
+                pokeDescription += "<h2>Height</h2>"
+                pokeDescription += "<p>" + pokemon.height + "</p>"
+                pokeDescription += "<h2>Weight</h2>"
+                pokeDescription += "<p>" + pokemon.weight + "</p>"
+
+            $("div.characteristics").html(pokeDescription);
+
+            });
+        });
+    });
+});
+        
+
+
+
+
+
+
+
+
+            // $("div.content").append("<img src='' alt='' id=" + i + ">")
+
+            // Use .html instead: Long string of images
+            // Append to a large html all image tags
+            // $("div.content").append("<img src='http://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/" + i + ".png' alt='' id=" + i + ">")
     
 
         // for(var i=1;i<10;i++){
@@ -14,34 +61,29 @@ $(document).ready(function(){
         //         $("#" + i).attr("src",pokemon.sprites.front_default);
         //     });
         // };
-    });
+    
 
 
-    $("img").click(function(){
-        console.log("You Clicked an image")
-        alert("You clicked an image!")
-    });
+    // $("img").click(function(){
+    //     console.log("You Clicked an image")
+    //     alert("You clicked an image!")
+    // });
 
-    $("img").on("click",function(){
-        alert(this.id);
-        var identity = this.id;
-       $.get("https://pokeapi.co/api/v2/pokemon/" + this.id + "/", function(pokemon) {
-            console.log(pokemon)
-            // console.log(pokemon.types[1].type.name)
-            $("div.characteristics").append("<h1>"+pokemon.name+"</h1>")
-            $("div.characteristics").append("<img src='http://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/" + identity + ".png'>")
-            $("div.characteristics").append("<h2>Types</h2>")
-            for(j=0; j<pokemon.types.length; j++){
-                // console.log("For Loop Type Name")
-                console.log(pokemon.types[j].type.name)
-                $("div.characteristics").append("<li>" + pokemon.types[j].type.name + "</li>")
-            };
-            $("div.characteristics").append("<h2>Height</h2>")
-            $("div.characteristics").append("<p>" + pokemon.height + "</p>")
-            $("div.characteristics").append("<h2>Weight</h2>")
-            $("div.characteristics").append("<p>" + pokemon.weight + "</p>")
-       }); 
-    });
+
+    //    $.get("https://pokeapi.co/api/v2/pokemon/" + this.id + "/", function(pokemon) {
+    //         console.log(pokemon)
+    //         // console.log(pokemon.types[1].type.name)
+    //         $("div.characteristics").append("<h1>"+pokemon.name+"</h1>")
+    //         $("div.characteristics").append("<img src='http://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/shiny/" + identity + ".png'>")
+    //         $("div.characteristics").append("<h2>Types</h2>")
+    //         for(j=0; j<pokemon.types.length; j++){
+    //             // console.log("For Loop Type Name")
+    //             console.log(pokemon.types[j].type.name)
+    //             $("div.characteristics").append("<li>" + pokemon.types[j].type.name + "</li>")
+    //         };
+
+    //    }); 
+    // });
 
 
 
@@ -49,5 +91,5 @@ $(document).ready(function(){
     //     console.log("Click Worked")
     //     // $("img id").val();
     // });
-});
+
 
